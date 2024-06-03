d63ce22eb53c144a18294b03011aeca9db667c2b
Eldar Hasanbasic <e.hasanbasic@cheqsite.com>
2024-06-03 12:52:28 +0200

    Add more options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export function getZoomLevel(): { zoomLevel: number, visualViewportScale: number } {
-    if (typeof window === 'undefined') {
-        throw new Error('window is not defined');
+export function getZoomLevel(): {
+    zoomLevelPercentage: number;
+    visualViewportScale: number;
+    zoomViaWindowDevicePixelRatio: number;
+    viewportZoomLevel: number;
+} {
+    if (typeof window === "undefined") {
+        throw new Error("window is not defined");
     }
 
     const devicePixelRatio = window.devicePixelRatio || 1;
@@ -8,10 +13,20 @@ export function getZoomLevel(): { zoomLevel: number, visualViewportScale: number
     const zoomLevel = (rect.width * devicePixelRatio) / window.innerWidth;
     const zoomLevelPercentage = Math.round(zoomLevel * 100);
 
-    let visualViewportScale = Math.round((window.outerWidth / window.innerWidth) * 100);
+    const visualViewportScale = Math.round(
+        (window.outerWidth / window.innerWidth) * 100
+    );
+    const zoomViaWindowDevicePixelRatio = 1 / devicePixelRatio;
+
+    let viewportZoomLevel = 0;
+    if (window.visualViewport) {
+        viewportZoomLevel = window.visualViewport.scale;
+    }
 
     return {
-        zoomLevel: zoomLevelPercentage,
-        visualViewportScale: visualViewportScale
+        zoomLevelPercentage,
+        visualViewportScale,
+        zoomViaWindowDevicePixelRatio,
+        viewportZoomLevel,
     };
 }
